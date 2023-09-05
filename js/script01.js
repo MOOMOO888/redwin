1fafb2b2b8f5b46cded0a03d3139cbf769c9201e
MOOMOO888 <S6552410003@sau.ac.th>
2023-09-05 17:15:21 +0700

    code v.5

---
+++ js/script01.js
@@ -0,0 +1,47 @@
+//alert('Hi........');
+//console.log("bbbbb"); console.log(1111)
+//console.log(30 + 20);
+
+//console.log (document.getElementById('num1').value)
+//n1 = document.getElementById('num1').value
+//n1 = document.getElementById('num1').value
+
+//alert( perselInt(n1) + perselInt(n2))
+
+function showDti(){
+    console.log('Welcomejub...')
+}
+let btnSum = document.getElementById("btn-sum")
+let showSum = document.getElementById("show-sum")
+let btnCancel = document.getElementById("btn-cancel")
+
+let num1 = document.getElementById("num1")
+let num2 = document.getElementById("num2")
+
+btnSum.addEventListener("click", function(){
+    let sum = parseFloat( num1.value ) + parseFloat( num2.value )
+    showSum.innerHTML = sum
+})
+
+
+btnCancel.addEventListener("click", function(){
+    num1.value=""
+    num2.value=""
+    showSum.innerHTM ="?????"
+})
+
+let btnRed = document.getElementById("btn-red")
+let btnGreen = document.getElementById("btn-green")
+let btnYellow = document.getElementById("btn-yellow")
+let Body = document.getElementById("body")
+
+
+btnRed.addEventListener("click", function(){
+   document.body.style.backgroundColor = "red"
+})
+btnGreen.addEventListener("click", function(){
+    document.body.style.backgroundColor = "green"
+})
+btnYellow.addEventListener("click", function(){
+    document.body.style.backgroundColor = "yellow"
+})
